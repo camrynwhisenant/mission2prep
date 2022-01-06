dd4befa505069f835625d53072b7946196ffeba8
camrynwhisenant <90424311+camrynwhisenant@users.noreply.github.com>
2022-01-06 13:21:11 -0700

    Initial check-in of module inclass1_5

---
+++ inclass1_5/wwwroot/js/fanmail.js
@@ -0,0 +1,15 @@
+﻿// document.getElementById("sendbtn").addEventListener("click", function () {
+ //   alert("From: " + document.getElementById("txtfrom").value +
+  //      ", Subject: " + document.getElementById("txtsubject").value +
+ //       ", Message: " + document.getElementById("txtmessage").value);
+//
+        
+//})
+
+$("#sendbtn").click( function () {
+    alert("From: " + $("#txtfrom").val() +
+        ", Subject: " + $("#txtsubject").val() +
+        ", Message: " + $("#txtmessage").val());
+    $("#picganderson").fadeOut("slow");
+
+})
